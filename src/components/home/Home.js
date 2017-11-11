81bf76ce97cea88d3ef1d70967bfdb84341e067b
gaofang <837682676@qq.com>
2017-11-11 10:40:37 +0800

    add home
    Signed-off-by: gaofang <837682676@qq.com>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,24 @@
 import styles from './Home.css';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className={styles.container}>
-        index
+        <button
+          className={styles.btn}
+          onClick={() => window.location.assign('test-snake.html')}
+        >Play a Game
+        </button>
+        <button
+          className={styles.btn}
+          onClick={() => window.location.assign('resume.html')}
+        >Look my Resume
+        </button>
+        <button
+          className={styles.btn}
+          onClick={() => window.location.assign('https://github.com/gaofang')}
+        >Back to my Github
+        </button>
       </div>
     );
   }
